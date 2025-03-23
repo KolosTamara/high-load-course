10e299fcf59605a92bbbef12ad4612775812efc1
Alina Ilyassova <alinail03@mail.ru>
2025-03-23 16:34:42 +0300

    case5

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -35,19 +35,23 @@ class PaymentExternalSystemAdapterImpl(
     private val requestAverageProcessingTime = properties.averageProcessingTime
     private val rateLimitPerSec = properties.rateLimitPerSec
     private val parallelRequests = properties.parallelRequests
+    private var maxTime = now() - now()
+    private var percentile = now()
+    private val recentPercentiles = ArrayDeque<Long>(100)
 
-    private var callTimeout = properties.averageProcessingTime.toMillis()
+    private var callTimeout = properties.averageProcessingTime.toMillis() * 2
     private var client = OkHttpClient.Builder().callTimeout(Duration.ofMillis(callTimeout)).build()
 
     private val histogram = Histogram(1, requestAverageProcessingTime.toMillis() * 2 , 2)
+
     private val rateLimiter = SlidingWindowRateLimiter(rate = rateLimitPerSec.toLong(), window = Duration.ofSeconds(1))
     private val semaphore = Semaphore((parallelRequests))
 
     override fun performPaymentAsync(paymentId: UUID, amount: Int, paymentStartedAt: Long, deadline: Long) {
-        logger.warn("[$accountName] Submitting payment request for payment $paymentId")
+        //logger.warn("[$accountName] Submitting payment request for payment $paymentId")
 
         val transactionId = UUID.randomUUID()
-        logger.info("[$accountName] Submit for $paymentId , txId: $transactionId")
+        //logger.info("[$accountName] Submit for $paymentId , txId: $transactionId")
 
         // Вне зависимости от исхода оплаты важно отметить что она была отправлена.
         // Это требуется сделать ВО ВСЕХ СЛУЧАЯХ, поскольку эта информация используется сервисом тестирования.
@@ -69,28 +73,32 @@ class PaymentExternalSystemAdapterImpl(
         }
         var shouldRetry = true;
 
-
         while (now() < deadline && shouldRetry) {
             if (semaphore.tryAcquire(processingTimeMillis, TimeUnit.SECONDS)) {
                 rateLimiter.tickBlocking()
                 try {
                     val requestStartTime = now()
-                    callTimeout = max(properties.averageProcessingTime.toMillis(), histogram.getValueAtPercentile(90.0))
-                    client = OkHttpClient.Builder().callTimeout(Duration.ofMillis(callTimeout)).build()
+                    callTimeout = histogram.getValueAtPercentile(90.0)
+                    client = OkHttpClient.Builder().callTimeout(Duration.ofMillis((callTimeout * 1.2).toLong())).build()
                     client.newCall(request).execute().use { response ->
                         val body = try {
                             mapper.readValue(response.body?.string(), ExternalSysResponse::class.java)
                         } catch (e: Exception) {
-                            logger.error("[$accountName] [ERROR] Payment processed for txId: $transactionId, payment: $paymentId, result code: ${response.code}, reason: ${response.body?.string()}")
+                            //logger.error("[$accountName] [ERROR] Payment processed for txId: $transactionId, payment: $paymentId, result code: ${response.code}, reason: ${response.body?.string()}")
                             ExternalSysResponse(transactionId.toString(), paymentId.toString(), false, e.message)
                         }
 
-                        logger.warn("[$accountName] Payment processed for txId: $transactionId, payment: $paymentId, succeeded: ${body.result}, message: ${body.message}")
-
-                        histogram.recordValue(now() - requestStartTime)
-
+                        //logger.warn("[$accountName] Payment processed for txId: $transactionId, payment: $paymentId, succeeded: ${body.result}, message: ${body.message}")
                         // Здесь мы обновляем состояние оплаты в зависимости от результата в базе данных оплат.
                         // Это требуется сделать ВО ВСЕХ ИСХОДАХ (успешная оплата / неуспешная / ошибочная ситуация)
+
+                        val requestProcessingTimeVal = now() - requestStartTime
+                        maxTime = max(requestProcessingTimeVal, maxTime)
+                        histogram.recordValue(now() - requestStartTime)
+                        percentile = histogram.getValueAtPercentile(90.0)
+
+                        logger.warn("[$accountName] - time: $requestProcessingTimeVal | avgTime: ${properties.averageProcessingTime.toMillis()} | maxTimeNow: $maxTime | percentile: $percentile")
+
                         paymentESService.update(paymentId) {
                             it.logProcessing(body.result, now(), transactionId, reason = body.message)
                         }
@@ -99,20 +107,20 @@ class PaymentExternalSystemAdapterImpl(
                 } catch (e: Exception) {
                     when (e) {
                         is SocketTimeoutException -> {
-                            logger.error(
+                            /*logger.error(
                                 "[$accountName] Payment timeout for txId: $transactionId, payment: $paymentId",
                                 e
-                            )
+                            )*/
                             paymentESService.update(paymentId) {
                                 it.logProcessing(false, now(), transactionId, reason = "Request timeout.")
                             }
                         }
 
                         else -> {
-                            logger.error(
+                            /*logger.error(
                                 "[$accountName] Payment failed for txId: $transactionId, payment: $paymentId",
                                 e
-                            )
+                            )*/
 
                             paymentESService.update(paymentId) {
                                 it.logProcessing(false, now(), transactionId, reason = e.message)
