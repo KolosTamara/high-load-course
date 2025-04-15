6cf2a99c3fcfd74daca245c6fdbadc814d827bc2
Tamara <tamarakolos.375@gmail.com>
2025-04-15 18:10:56 +0300

    OkHttpClient

--- README.md
+++ README.md
@@ -1,22 +1,9 @@
-# Template for the HighLoad course
-This project is based on [Tiny Event Sourcing library](https://github.com/andrsuh/tiny-event-sourcing)
+OkHttpClient + execute
+![img.png](img.png)
+![img_1.png](img_1.png)
 
-### Run PostgreSql
-This example uses Postgres as an implementation of the Event store. You can see it in `pom.xml`:
 
-```
-<dependency>
-    <groupId>ru.quipy</groupId>
-    <artifactId>tiny-postgres-event-store-spring-boot-starter</artifactId>
-    <version>${tiny.es.version}</version>
-</dependency>
-```
-
-Thus, you have to run Postgres in order to test this example. We have `docker-compose` file in the root. Run following command to start the database:
-
-```
-docker-compose up
-```
-
-### Run the application
-To make the application run you can start the main class `OnlineShopApplication`.
+OkHttpClient + enqueue + rateLimiter
+![img_2.png](img_2.png)
+![img_3.png](img_3.png)
+![img_4.png](img_4.png)
--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -1,32 +1,26 @@
 package ru.quipy.payments.logic
-
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody
+import okhttp3.*
 import org.HdrHistogram.Histogram
 import org.slf4j.LoggerFactory
 import ru.quipy.core.EventSourcingService
 import ru.quipy.payments.api.PaymentAggregate
 import java.net.SocketTimeoutException
 import ru.quipy.common.utils.SlidingWindowRateLimiter
+import java.io.IOException
 import java.time.Duration
 import java.util.*
 import java.util.concurrent.Semaphore
 import java.util.concurrent.TimeUnit
 import kotlin.math.max
-
-
 // Advice: always treat time as a Duration
 class PaymentExternalSystemAdapterImpl(
     private val properties: PaymentAccountProperties,
     private val paymentESService: EventSourcingService<UUID, PaymentAggregate, PaymentAggregateState>
 ) : PaymentExternalSystemAdapter {
-
     companion object {
         val logger = LoggerFactory.getLogger(PaymentExternalSystemAdapter::class.java)
-
         val emptyBody = RequestBody.create(null, ByteArray(0))
         val mapper = ObjectMapper().registerKotlinModule()
     }
@@ -38,103 +32,66 @@ class PaymentExternalSystemAdapterImpl(
     private var maxTime = now() - now()
     private var percentile = now()
     private val recentPercentiles = ArrayDeque<Long>(100)
-
     private var callTimeout = properties.averageProcessingTime.toMillis() * 2
-    private var client = OkHttpClient.Builder().callTimeout(Duration.ofMillis(callTimeout)).build()
-
+    private var client = OkHttpClient.Builder().protocols(listOf(Protocol.HTTP_2, Protocol.HTTP_1_1))
+        .connectionPool(ConnectionPool(1000, 1, TimeUnit.MINUTES))
+        .dispatcher(Dispatcher().apply {
+            maxRequests = parallelRequests
+            maxRequestsPerHost = parallelRequests
+        })
+        .callTimeout(Duration.ofMillis(requestAverageProcessingTime.toMillis() * 2))
+        .build()
     private val histogram = Histogram(1, requestAverageProcessingTime.toMillis() * 2 , 2)
-
     private val rateLimiter = SlidingWindowRateLimiter(rate = rateLimitPerSec.toLong(), window = Duration.ofSeconds(1))
     private val semaphore = Semaphore((parallelRequests))
-
     override fun performPaymentAsync(paymentId: UUID, amount: Int, paymentStartedAt: Long, deadline: Long) {
         //logger.warn("[$accountName] Submitting payment request for payment $paymentId")
-
         val transactionId = UUID.randomUUID()
         //logger.info("[$accountName] Submit for $paymentId , txId: $transactionId")
-
         // Вне зависимости от исхода оплаты важно отметить что она была отправлена.
         // Это требуется сделать ВО ВСЕХ СЛУЧАЯХ, поскольку эта информация используется сервисом тестирования.
         paymentESService.update(paymentId) {
             it.logSubmission(success = true, transactionId, now(), Duration.ofMillis(now() - paymentStartedAt))
         }
-
         val request = Request.Builder().run {
             url("http://localhost:1234/external/process?serviceName=${serviceName}&accountName=${accountName}&transactionId=$transactionId&paymentId=$paymentId&amount=$amount")
             post(emptyBody)
         }.build()
-
-        val processingTimeMillis = deadline - now()
-        var shouldRetry = true;
-
-        while (now() < deadline && shouldRetry) {
-            if (semaphore.tryAcquire(processingTimeMillis, TimeUnit.SECONDS)) {
-                rateLimiter.tickBlocking()
-                try {
-                    val requestStartTime = now()
-                    callTimeout = histogram.getValueAtPercentile(90.0)
-                    client = OkHttpClient.Builder().callTimeout(Duration.ofMillis((callTimeout * 1.2).toLong())).build()
-                    client.newCall(request).execute().use { response ->
-                        val body = try {
-                            mapper.readValue(response.body?.string(), ExternalSysResponse::class.java)
-                        } catch (e: Exception) {
-                            //logger.error("[$accountName] [ERROR] Payment processed for txId: $transactionId, payment: $paymentId, result code: ${response.code}, reason: ${response.body?.string()}")
-                            ExternalSysResponse(transactionId.toString(), paymentId.toString(), false, e.message)
-                        }
-
-                        //logger.warn("[$accountName] Payment processed for txId: $transactionId, payment: $paymentId, succeeded: ${body.result}, message: ${body.message}")
-                        // Здесь мы обновляем состояние оплаты в зависимости от результата в базе данных оплат.
-                        // Это требуется сделать ВО ВСЕХ ИСХОДАХ (успешная оплата / неуспешная / ошибочная ситуация)
-
-                        val requestProcessingTimeVal = now() - requestStartTime
-                        maxTime = max(requestProcessingTimeVal, maxTime)
-                        histogram.recordValue(now() - requestStartTime)
-                        percentile = histogram.getValueAtPercentile(90.0)
-
-                        logger.warn("[$accountName] - time: $requestProcessingTimeVal | avgTime: ${properties.averageProcessingTime.toMillis()} | maxTimeNow: $maxTime | percentile: $percentile")
-
-                        paymentESService.update(paymentId) {
-                            it.logProcessing(body.result, now(), transactionId, reason = body.message)
-                        }
-                        shouldRetry = !body.result
-                    }
-                } catch (e: Exception) {
-                    when (e) {
-                        is SocketTimeoutException -> {
-                            /*logger.error(
-                                "[$accountName] Payment timeout for txId: $transactionId, payment: $paymentId",
-                                e
-                            )*/
-                            paymentESService.update(paymentId) {
-                                it.logProcessing(false, now(), transactionId, reason = "Request timeout.")
-                            }
-                        }
-
-                        else -> {
-                            /*logger.error(
-                                "[$accountName] Payment failed for txId: $transactionId, payment: $paymentId",
-                                e
-                            )*/
-
-                            paymentESService.update(paymentId) {
-                                it.logProcessing(false, now(), transactionId, reason = e.message)
-                            }
-                        }
-                    }
-                    shouldRetry = true
-                } finally {
-                    semaphore.release()
+//        val processingTimeMillis = deadline - now()
+//        var shouldRetry = true;
+        if (!rateLimiter.tick()) {
+            logger.warn("Dropped due to rate limit")
+            paymentESService.update(paymentId) {
+                it.logProcessing(false, now(), transactionId, reason = "Dropped due to rate limit")
+            }
+            return
+        }
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                val reason = when (e) {
+                    is SocketTimeoutException -> "Request timeout."
+                    else -> e.message ?: "Unknown error."
+                }
+                paymentESService.update(paymentId) {
+                    it.logProcessing(false, now(), transactionId, reason = reason)
                 }
             }
-        }
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    val body = try {
+                        mapper.readValue(response.body?.string(), ExternalSysResponse::class.java)
+                    } catch (e: Exception) {
+                        ExternalSysResponse(transactionId.toString(), paymentId.toString(), false, e.message)
+                    }
+                    paymentESService.update(paymentId) {
+                        it.logProcessing(body.result, now(), transactionId, reason = body.message)
+                    }
+                }
+            }
+        })
     }
-
     override fun price() = properties.price
-
     override fun isEnabled() = properties.enabled
-
     override fun name() = properties.accountName
-
 }
-
 public fun now() = System.currentTimeMillis()
