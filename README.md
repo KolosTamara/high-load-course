13ab6ea7097bd611adbe54a47bee94897791006e
Tamara <tamarakolos.375@gmail.com>
2025-04-15 18:17:19 +0300

    case7 httpClient

--- README.md
+++ README.md
@@ -1,9 +1,4 @@
-OkHttpClient + execute
-![img.png](img.png)
-![img_1.png](img_1.png)
-
-
-OkHttpClient + enqueue + rateLimiter
-![img_2.png](img_2.png)
-![img_3.png](img_3.png)
-![img_4.png](img_4.png)
+HttpClient
+![img_6.png](img_6.png)
+![img_7.png](img_7.png)
+![img_8.png](img_8.png)
--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -1,27 +1,30 @@
 package ru.quipy.payments.logic
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-import okhttp3.*
 import org.HdrHistogram.Histogram
 import org.slf4j.LoggerFactory
 import ru.quipy.core.EventSourcingService
 import ru.quipy.payments.api.PaymentAggregate
-import java.net.SocketTimeoutException
 import ru.quipy.common.utils.SlidingWindowRateLimiter
 import java.io.IOException
+import java.net.URI
+import java.net.SocketTimeoutException
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.net.http.HttpTimeoutException
 import java.time.Duration
 import java.util.*
+import java.util.concurrent.CompletableFuture
 import java.util.concurrent.Semaphore
-import java.util.concurrent.TimeUnit
 import kotlin.math.max
-// Advice: always treat time as a Duration
 class PaymentExternalSystemAdapterImpl(
     private val properties: PaymentAccountProperties,
     private val paymentESService: EventSourcingService<UUID, PaymentAggregate, PaymentAggregateState>
 ) : PaymentExternalSystemAdapter {
     companion object {
         val logger = LoggerFactory.getLogger(PaymentExternalSystemAdapter::class.java)
-        val emptyBody = RequestBody.create(null, ByteArray(0))
         val mapper = ObjectMapper().registerKotlinModule()
     }
     private val serviceName = properties.serviceName
@@ -32,66 +35,60 @@ class PaymentExternalSystemAdapterImpl(
     private var maxTime = now() - now()
     private var percentile = now()
     private val recentPercentiles = ArrayDeque<Long>(100)
-    private var callTimeout = properties.averageProcessingTime.toMillis() * 2
-    private var client = OkHttpClient.Builder().protocols(listOf(Protocol.HTTP_2, Protocol.HTTP_1_1))
-        .connectionPool(ConnectionPool(1000, 1, TimeUnit.MINUTES))
-        .dispatcher(Dispatcher().apply {
-            maxRequests = parallelRequests
-            maxRequestsPerHost = parallelRequests
-        })
-        .callTimeout(Duration.ofMillis(requestAverageProcessingTime.toMillis() * 2))
+    private val client = HttpClient.newBuilder()
+        .version(HttpClient.Version.HTTP_2)
+        .connectTimeout(Duration.ofMillis(requestAverageProcessingTime.toMillis()*2))
+        .executor(java.util.concurrent.Executors.newFixedThreadPool(1000))
         .build()
     private val histogram = Histogram(1, requestAverageProcessingTime.toMillis() * 2 , 2)
     private val rateLimiter = SlidingWindowRateLimiter(rate = rateLimitPerSec.toLong(), window = Duration.ofSeconds(1))
     private val semaphore = Semaphore((parallelRequests))
     override fun performPaymentAsync(paymentId: UUID, amount: Int, paymentStartedAt: Long, deadline: Long) {
-        //logger.warn("[$accountName] Submitting payment request for payment $paymentId")
         val transactionId = UUID.randomUUID()
-        //logger.info("[$accountName] Submit for $paymentId , txId: $transactionId")
-        // Вне зависимости от исхода оплаты важно отметить что она была отправлена.
-        // Это требуется сделать ВО ВСЕХ СЛУЧАЯХ, поскольку эта информация используется сервисом тестирования.
         paymentESService.update(paymentId) {
             it.logSubmission(success = true, transactionId, now(), Duration.ofMillis(now() - paymentStartedAt))
         }
-        val request = Request.Builder().run {
-            url("http://localhost:1234/external/process?serviceName=${serviceName}&accountName=${accountName}&transactionId=$transactionId&paymentId=$paymentId&amount=$amount")
-            post(emptyBody)
-        }.build()
-//        val processingTimeMillis = deadline - now()
-//        var shouldRetry = true;
-        if (!rateLimiter.tick()) {
-            logger.warn("Dropped due to rate limit")
-            paymentESService.update(paymentId) {
-                it.logProcessing(false, now(), transactionId, reason = "Dropped due to rate limit")
+//        if (!rateLimiter.tick()) {
+//            logger.warn("Dropped due to rate limit")
+//            paymentESService.update(paymentId) {
+//                it.logProcessing(false, now(), transactionId, reason = "Dropped due to rate limit")
+//            }
+//            return
+//        }
+        val uri = URI.create("http://localhost:1234/external/process?serviceName=$serviceName&accountName=$accountName&transactionId=$transactionId&paymentId=$paymentId&amount=$amount")
+        val request = HttpRequest.newBuilder()
+            .uri(uri)
+            .timeout(Duration.ofMillis(requestAverageProcessingTime.toMillis() * 2))
+            .POST(HttpRequest.BodyPublishers.noBody())
+            .build()
+
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+            .thenApply { response ->
+                try {
+                    val body = mapper.readValue<ExternalSysResponse>(response.body())
+                    paymentESService.update(paymentId) {
+                        it.logProcessing(body.result, now(), transactionId, reason = body.message)
+                    }
+                } catch (e: Exception) {
+                    paymentESService.update(paymentId) {
+                        it.logProcessing(false, now(), transactionId, reason = e.message)
+                    }
+                }
             }
-            return
-        }
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                val reason = when (e) {
-                    is SocketTimeoutException -> "Request timeout."
-                    else -> e.message ?: "Unknown error."
+            .exceptionally { ex ->
+                val reason = when (ex.cause) {
+                    is HttpTimeoutException -> "Request timeout."
+                    is IOException -> "I/O Error: ${ex.message}"
+                    else -> ex.message ?: "Unknown error."
                 }
                 paymentESService.update(paymentId) {
                     it.logProcessing(false, now(), transactionId, reason = reason)
                 }
+                null
             }
-            override fun onResponse(call: Call, response: Response) {
-                response.use {
-                    val body = try {
-                        mapper.readValue(response.body?.string(), ExternalSysResponse::class.java)
-                    } catch (e: Exception) {
-                        ExternalSysResponse(transactionId.toString(), paymentId.toString(), false, e.message)
-                    }
-                    paymentESService.update(paymentId) {
-                        it.logProcessing(body.result, now(), transactionId, reason = body.message)
-                    }
-                }
-            }
-        })
     }
     override fun price() = properties.price
     override fun isEnabled() = properties.enabled
     override fun name() = properties.accountName
 }
-public fun now() = System.currentTimeMillis()
+fun now() = System.currentTimeMillis()
